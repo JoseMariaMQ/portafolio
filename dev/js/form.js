3247cf16474e479192fb0587c703d5b0d24dbace
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2020-09-24 17:57:41 +0200

    Change skills aparence, include contact_process.php

--- dev/js/form.js
+++ dev/js/form.js
@@ -1,7 +1,6 @@
 const submit = document.getElementById('contact-submit')
 const contactMessage = document.getElementById('contact-message')
 
-submit.addEventListener('submit', (e) => {
-    e.preventDefault()
+submit.addEventListener('click', () => {
     contactMessage.innerText = '¡Gracias por contactar conmigo!'
 })
