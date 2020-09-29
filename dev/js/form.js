4efa143a8162c705e8b9a0dd4ebe45383f00b97f
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2020-09-29 11:20:59 +0200

    Include img project To do list

--- dev/js/form.js
+++ dev/js/form.js
@@ -1,6 +1,8 @@
-const submit = document.getElementById('contact-submit')
 const contactMessage = document.getElementById('contact-message')
 
-submit.addEventListener('click', () => {
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
     contactMessage.innerText = '¡Gracias por contactar conmigo!'
 })
